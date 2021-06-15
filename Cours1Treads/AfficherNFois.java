a90be6d9d2fbb0e21b7fc05fa7052aab75d910d8
blacol <anthonycolvil@gmail.com>
2021-06-15 11:02:34 +0200

    optimisation

--- Cours1Treads/AfficherNFois.java
+++ Cours1Treads/AfficherNFois.java
@@ -1,18 +1,26 @@
 public class AfficherNFois extends Thread {
     private int nb;
     private String meesage;
+    private int interation;
+    private int w ;
 
-    public AfficherNFois (int n, String m){
-        nb=n;
+    public AfficherNFois (int commencement, String m, int fin){
+        nb = fin;
         meesage=m;
+        interation = 0;
+        w = commencement;
     }
 
     public void run() {
         try{
-            for (int i=0; i<nb; i++){
-                System.out.println(meesage);
+            for (int i= w; i<nb; i++){
+                if(meesage.charAt(i)=='e'){
+                    interation++;
+                }
                 //sleep(1000);
             }
+            //System.out.println(interation);
+
         }
         catch (Exception e){}
     }
--- Cours1Treads/main.java
+++ Cours1Treads/main.java
@@ -2,10 +2,41 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
+        int debut;
+        String message;
+        int z ;
+        int fin ;
+        int y = 1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir Le nombre de threads :");
+        int maxThreads = sc.nextInt();
+
+        message = "Veuillezibiibiubiububiuivuiyiviyvyvyviviuvuejiibiububububub";
+        
+// Optimiser avec une tab de achage
+
+        AfficherNFois tab [] = new AfficherNFois[maxThreads];
+			for(int i=0; i<maxThreads; i++){
+                
+                fin = message.length()/z;
+                debut = message.length()/y;
+
+				tab[i]= new AfficherNFois(debut, message,fin);
+			}
+			for(int i=0; i<maxThreads; i++){
+				tab[i].start();
+                try {
+                    tab[i].join();
+                } catch (InterruptedException e) {
+                }
+			}
+
+        /*
         AfficherNFois a1=new AfficherNFois(26, "Bonjour");
         AfficherAlphabet b2 = new AfficherAlphabet();
         a1.start();
-        b2.start();
+        b2.start(); */
     }
     
 }
